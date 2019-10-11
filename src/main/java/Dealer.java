342a0a395dc11061f9804063cbe592b6c6eb2abf
hayden <haydenwb@gmail.com>
2019-10-11 12:25:31 +0100

    created arraylist for dealer, now creating test

--- src/main/java/Dealer.java
+++ src/main/java/Dealer.java
@@ -1,11 +1,16 @@
+import java.util.ArrayList;
+
 public class Dealer {
     private String name;
+    private ArrayList<Card> hand;
 
     public Dealer(String name) {
         this.name = name;
+        this.hand = new ArrayList<Card>();
     }
 
     public String getName() {
         return this.name;
     }
+
 }
